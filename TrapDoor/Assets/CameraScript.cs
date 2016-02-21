b845c88773bd8625d415053a45bbbdae7669dd19
Andrew Abriam <a_abriam@outlook.com>
2016-02-20 23:02:56 -0800

    Camera tweaks

---
+++ TrapDoor/Assets/CameraScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+
+    public GameObject player;
+    private Vector3 followPos;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 follow = new Vector3(transform.position.x, transform.position.y, player.transform.position.z + 15);
+        followPos = Vector3.Lerp(transform.position, follow, Time.deltaTime * 2);
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, followPos.z);
+    }
+}
