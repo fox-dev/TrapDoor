78b6aa6f94e05cfbc725b4b38503c17ea510fce5
OrphenD <AQuach89@gmail.com>
2016-02-21 02:17:16 -0800

    Added laser object

---
+++ TrapDoor/Assets/LaserScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserScript : MonoBehaviour {
+
+	LineRenderer line;
+	int layerMask;
+
+	// Use this for initialization
+	void Start () 
+	{
+		line = gameObject.GetComponent<LineRenderer> ();
+		line.enabled = true;
+
+		layerMask = 1 << 8;
+
+		layerMask = ~layerMask;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		Ray ray = new Ray (transform.position, transform.forward);
+		RaycastHit hit;
+
+		line.SetPosition (0, ray.origin);
+		if (Physics.Raycast (ray, out hit, 100, layerMask)) {
+			line.SetPosition (1, hit.point);
+			if (hit.collider.tag == "Player") {
+				print ("I hit the player");
+			}
+		}
+		else
+			line.SetPosition (1, ray.GetPoint (100));
+	}
+}
