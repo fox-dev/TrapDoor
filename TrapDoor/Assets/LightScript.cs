18e07c44af052b214230f0d3af5c6ee7923608ca
OrphenD <AQuach89@gmail.com>
2016-03-13 00:30:14 -0800

    Added more visual lights blinking, Added some easy sets

---
+++ TrapDoor/Assets/LightScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightScript : MonoBehaviour {
+
+	public float fadeSpeed = 2f;
+	public float highIntensity = 2f;
+	public float lowIntensity = 0.5f;
+	public float changeMargin = 0.2f;
+
+	public Light myLight;
+	private float targetIntensity;
+
+
+	//public float fadeSpeedShader = 2f;
+	public float highIntensityShader = 1f;
+	public float lowIntensityShader = 1.03f;
+	//public float changeMarginShader = 0.002f;
+
+	public Renderer rend;
+	private float shaderTarget, scaleX;
+
+	// Use this for initialization
+	void Start () 
+	{
+		myLight.intensity = lowIntensity;
+
+		scaleX = lowIntensityShader;
+		rend.material.mainTextureScale = new Vector2(scaleX, 1f);
+
+		shaderTarget = highIntensityShader;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		myLight.intensity = Mathf.Lerp (myLight.intensity, targetIntensity, fadeSpeed * Time.deltaTime);
+		scaleX = Mathf.Lerp (scaleX, shaderTarget, fadeSpeed * Time.deltaTime);
+		rend.material.mainTextureScale = new Vector2(scaleX, 1f);
+		CheckTargetIntensity ();
+	}
+
+	void CheckTargetIntensity()
+	{	
+		if (Mathf.Abs (targetIntensity - myLight.intensity) < changeMargin) {
+			if (targetIntensity == highIntensity) {
+				targetIntensity = lowIntensity;
+				shaderTarget = lowIntensityShader;
+			} else {
+				targetIntensity = highIntensity;
+				shaderTarget = highIntensityShader;
+			}
+		}
+	}
+
+}
---
+++ TrapDoor/Assets/LightShaderScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightShaderScript : MonoBehaviour {
+
+	public float fadeSpeed = 2f;
+	public float highIntensity = 1f;
+	public float lowIntensity = 1.03f;
+	public float changeMargin = 0.002f;
+
+	public Renderer rend;
+
+	private float targetIntensity, scaleX;
+
+	// Use this for initialization
+	void Start () 
+	{
+		scaleX = lowIntensity;
+		rend.material.mainTextureScale = new Vector2(scaleX, 1f);
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		scaleX = Mathf.Lerp (scaleX, targetIntensity, fadeSpeed * Time.deltaTime);
+		rend.material.mainTextureScale = new Vector2(scaleX, 1f);
+		CheckTargetIntensity ();
+	}
+
+	void CheckTargetIntensity()
+	{	
+		if (Mathf.Abs (targetIntensity - scaleX) < changeMargin) {
+			if (targetIntensity == highIntensity) {
+				targetIntensity = lowIntensity;
+			} else {
+				targetIntensity = highIntensity;
+			}
+		}
+	}
+}
