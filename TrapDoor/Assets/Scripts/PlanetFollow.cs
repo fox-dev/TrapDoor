b8c0b6f699bd018be01610439338162a5f0ef70a
Andrew Abriam <a_abriam@outlook.com>
2016-03-16 02:52:24 -0700

    Fixed planet follow script

--- TrapDoor/Assets/Scripts/PlanetFollow.cs
+++ TrapDoor/Assets/Scripts/PlanetFollow.cs
@@ -5,10 +5,22 @@ public class PlanetFollow : MonoBehaviour {
 
     public GameObject player;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    private RotateManager rotateTracker;
+
+    // Use this for initialization
+    void Start () {
+
+        GameObject rotateTrackerObject = GameObject.FindWithTag("Rotator");
+        if (rotateTrackerObject != null)
+        {
+            rotateTracker = rotateTrackerObject.GetComponent<RotateManager>();
+        }
+        if (rotateTracker == null)
+        {
+            Debug.Log("Cannot find 'RotateManager' script");
+        }
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -20,7 +32,26 @@ public class PlanetFollow : MonoBehaviour {
         if(other.tag == "Player")
         {
             print("leaving");
-            transform.position = new Vector3(transform.position.x, transform.position.y, player.transform.position.z*2);
+            if(rotateTracker.getOrientation() == "down")
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, player.transform.position.z + 1300);
+            }
+            else if(rotateTracker.getOrientation() == "up")
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, player.transform.position.z - 1300);
+            }
+            else if (rotateTracker.getOrientation() == "left")
+            {
+                transform.position = new Vector3(transform.position.x - 2800, transform.position.y, transform.position.z);
+            }
+            else if (rotateTracker.getOrientation() == "right")
+            {
+                transform.position = new Vector3(transform.position.x + 2800, transform.position.y, transform.position.z);
+            }
+            
+
+
+
 
         }
 
