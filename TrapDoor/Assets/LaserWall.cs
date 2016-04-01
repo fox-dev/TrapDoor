b7eb6954cefa3d82534c305be056e296b33764da
OrphenD <AQuach89@gmail.com>
2016-03-31 23:55:56 -0700

    Added new junction that randomizes walls.  Walls will do damage and sap super

---
+++ TrapDoor/Assets/LaserWall.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserWall : MonoBehaviour {
+
+	private GameController gameController;
+
+	// Use this for initialization
+	void Start () 
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+		if (gameControllerObject != null) {
+			gameController = gameControllerObject.GetComponent<GameController>();
+		}
+		if (gameController == null)
+		{
+			Debug.Log("Cannot find 'GameController' script");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			other.GetComponent<PlayerMovement>().blink();
+			gameController.decBoost (20);
+			other.GetComponent<Rigidbody>().drag = 15;
+
+		}
+
+
+	}
+}
---
+++ TrapDoor/Assets/WallRandomizer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WallRandomizer : MonoBehaviour 
+{
+
+	public List<GameObject> walls;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnEnable()
+	{
+		int opening = Random.Range (0, walls.Count);
+		for(int i = 0; i < walls.Count; i++)
+		{
+			if (i == opening) {
+				walls [i].SetActive (false);
+			} else {
+				walls [i].SetActive (true);
+			}
+		}
+	}
+}
