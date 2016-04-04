6bbf9686335eba45727da5debbc4bc464d6f0340
OrphenD <AQuach89@gmail.com>
2016-04-04 00:06:38 -0700

    Ad Controller Script to test

---
+++ TrapDoor/Assets/Scripts/Menu/AdController.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+using GoogleMobileAds.Api;
+
+public class AdController : MonoBehaviour {
+
+
+	private BannerView bannerView;
+	private InterstitialAd interstitial;
+
+	bool showAds;
+
+	// Use this for initialization
+	void Start () 
+	{
+		updateAdOptions();
+
+		RequestBanner ();
+		RequestInterstitial();
+
+		showBannerAd ();
+		showIntAd ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void updateAdOptions()
+	{
+		if (PlayerPrefs.GetString ("ads") == "true") {
+			showAds = true;
+		} else if (PlayerPrefs.GetString ("ads") == "false") {
+			showAds = false;
+		} else { //default
+			showAds = true;
+		}
+	}
+
+	private void RequestBanner()
+	{
+		string adUnitId = "ca-app-pub-8558533109120159/9957174021";
+
+		// Create a 320x50 banner at the top of the screen.
+		bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+		// Create an empty ad request.
+		AdRequest request = new AdRequest.Builder()
+			.AddTestDevice(AdRequest.TestDeviceSimulator)       // Simulator.
+			.AddTestDevice("96FC1D71712CF55B38F3DFBEED13E36C")  // My test device.
+			.Build();
+		// Load the banner with the request.
+		bannerView.LoadAd(request);
+	}
+
+	private void RequestInterstitial()
+	{
+		//Unity Android Ad ID
+		string adUnitId = "ca-app-pub-8558533109120159/243390722";
+
+		// Initialize an InterstitialAd.
+		interstitial = new InterstitialAd(adUnitId);
+		// Create an empty ad request.
+		AdRequest request = new AdRequest.Builder()
+			.AddTestDevice(AdRequest.TestDeviceSimulator)       // Simulator.
+			.AddTestDevice("96FC1D71712CF55B38F3DFBEED13E36C")  // My test device.
+			.Build();
+		// Load the interstitial with the request.
+		interstitial.LoadAd(request);
+	}
+
+
+	//Interstitial Ad stuff
+	public bool adIsLoaded()
+	{
+		return interstitial.IsLoaded ();
+	}
+
+	public void showIntAd()
+	{
+		interstitial.Show();
+	}
+		
+	//BannerAd Stuff
+	public void hideBannerAd()
+	{
+		bannerView.Hide ();
+	}
+
+	public void showBannerAd()
+	{
+		bannerView.Show ();
+	}
+	
+	//Ad flag stuff
+	public void turnOffAds()
+	{
+		showAds = false;
+		PlayerPrefs.SetString("ads", "false");
+	}
+
+	public void turnOnAds()
+	{
+		showAds = true;
+		PlayerPrefs.SetString("ads", "true");
+	}
+
+	public bool AdFlag()
+	{
+		return showAds;
+	}
+
+
+}
