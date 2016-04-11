cc67d5d0e7adda92d3636bbac564d341631ac87c
OrphenD <AQuach89@gmail.com>
2016-04-11 00:43:16 -0700

    - added new laser wall texture, texture scrolls, added BGM

---
+++ TrapDoor/Assets/Main/LazerWallAnimation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LazerWallAnimation : MonoBehaviour {
+
+	Renderer wall;
+
+	// Use this for initialization
+	void Start () {
+	
+		wall = gameObject.GetComponent<Renderer> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		wall.material.mainTextureOffset = new Vector2 (Time.time, 0);
+	
+	}
+}
